d155ca76a3b51f4fe82479bd108012a0d1db0644
pantelis <pampatzoglou@gmail.com>
2020-03-10 14:35:31 +0200

    test namespace

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 /scripts/deploy.sh -t helm -c engineering -a "\
+ --set namespace=matrix-recorder\
  --set image.tag=${CIRCLE_TAG}\
  --set baseUrl=$BASE_URL\
  --set accessToken=$W3F_BACKUPBOT_ACCESS_TOKEN\
