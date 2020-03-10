26aeb0b693bad1939c2aebdd33a5a067552ca253
pantelis <pampatzoglou@gmail.com>
2020-03-10 11:39:03 +0200

    test deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+/scripts/deploy.sh -t helm -c engineering -a "\
+ --set image.tag=${CIRCLE_TAG}\
+ --set github.client=$GITHUB_CLIENT\
+ --set baseUrl=$BASE_URL\
+ --set accessToken=$W3F_BACKUPBOT_ACCESS_TOKEN\
+ --set deviceId=$W3F_BACKUPBOT_DEVICE_ID\
+ --set userId=$W3F_BACKUPBOT_USER_ID\
+ matrix-recorder ./charts/ matrix-recorder/."
