864b337d197dea7325cf1d685b0476935ca0152f
pantelis <pampatzoglou@gmail.com>
2020-03-16 14:30:06 +0200

    test

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -8,24 +8,27 @@ run_tests() {
     echo Running tests...
 
     wait_pod_ready matrix-recorder
-    integration_test_sequence
 }
-
-integration_test_sequence(){
-  echo Running integration tests...
-  matrixrecorder_integration
-}
-
-matrixrecorder_integration(){
-  wait_pod_ready matrix-recorder-0 default 2/2
-}
-
 
 teardown() {
-    helmfile delete
+    helm delete matrix-recorder
 }
 
 main(){
+    if [ -z "$KEEP_W3F_MATRIX_REC" ]; then
+        trap teardown EXIT
+    fi
+
+    /scripts/build-helm.sh \
+        --set environment=ci \
+        --set baseUrl="${BASE_URL}" \
+        --set accessToken="${W3F_BACKUPBOT_ACCESS_TOKEN}" \
+        --set deviceId="${W3F_BACKUPBOT_DEVICE_ID}" \
+        --set userId="${W3F_BACKUPBOT_USER_ID}" \
+        --set image.tag=${CIRCLE_SHA1} \
+        matrix-recorder \
+        ./charts/matrix-recorder
+
     run_tests
 }
 
